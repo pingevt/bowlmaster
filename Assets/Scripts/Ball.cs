0edc1010ffba8f98614d61fa052f0a2fd0eec6e0
Pete Inge <pete.inge@gmail.com>
2017-04-20 19:50:09 -0400

    Make the ball move and add sound

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public float launchSpeed;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity = new Vector3 (0, 0, launchSpeed);
+		audioSource.Play ();
+	}
+}
